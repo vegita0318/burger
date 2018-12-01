623f70b82ef51ae6ec0f782545f9ad1618856cb7
Larry Fuller <lfullerj@gmail.com>
2018-12-01 08:55:28 -0500

    updates

---
+++ db/seeds.sql
@@ -0,0 +1,6 @@
+INSERT INTO burgers(burger_name, devoured)
+	values('GaSou Eagle Burger', false),
+    ('Double Spam Burger', false),
+    ('Sunnyside Egg Burger', false);
+    
+    select * from burgers;
